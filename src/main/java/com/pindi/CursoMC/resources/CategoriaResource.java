3bdd6c6818bf8139df76636f2eef36fcd92d0aa4
Ntunitangua <sckytopindi1981@gmail.com>
2019-11-26 15:17:03 +0000

    versão3 testado

--- src/main/java/com/pindi/CursoMC/resources/CategoriaResource.java
+++ src/main/java/com/pindi/CursoMC/resources/CategoriaResource.java
@@ -3,12 +3,28 @@ package com.pindi.CursoMC.resources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.pindi.CursoMC.domain.Categoria;
+
+
+
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	
+	public List<Categoria> listar() {
+		
+		Categoria cat1= new Categoria(1,"Informática");
+		Categoria cat2= new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
